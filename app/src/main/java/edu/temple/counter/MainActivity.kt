3bef4dbd51330010c0128ae2864ad64d8c39075e
beige <markoguaman45@gmail.com>
2026-04-22 16:01:12 -0400

    completed

--- app/src/main/java/edu/temple/counter/MainActivity.kt
+++ app/src/main/java/edu/temple/counter/MainActivity.kt
@@ -8,11 +8,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,12 +44,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Counter(modifier: Modifier = Modifier) {
-    Column (modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally) {
+    var count by rememberSaveable { mutableIntStateOf(0) }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { count++ }) {
+            Icon(
+                painter = painterResource(id = android.R.drawable.arrow_up_float),
+                contentDescription = "Increment",
+                modifier = Modifier.size(32.dp)
+            )
+        }
         Text(
-            text = "0",
-            fontSize = 192.sp
+            text = count.toString(),
+            fontSize = 192.sp,
+            modifier = Modifier.padding(vertical = 16.dp)
         )
+        Button(onClick = { count-- }) {
+            Icon(
+                painter = painterResource(id = android.R.drawable.arrow_down_float),
+                contentDescription = "Decrement",
+                modifier = Modifier.size(32.dp)
+            )
+        }
     }
 }
 
